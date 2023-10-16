bc421033c111e3cf0f599efa8fce64715af1ab9d
mohd shamsheer <143681879+mshamsheer@users.noreply.github.com>
2023-10-17 01:26:04 +0530

    Add files via upload

---
+++ indx.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".side-bar");
+let container = document.querySelector(".container");
+menuIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle('large-container')
+}
